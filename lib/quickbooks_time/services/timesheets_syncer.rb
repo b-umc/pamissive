f960470836f90946e2384ab870579710f96afb84
b-umc <benumc@gmail.com>
2025-08-19 08:16:35 -0700

    Fix auth loader race

--- lib/quickbooks_time/services/timesheets_syncer.rb
+++ lib/quickbooks_time/services/timesheets_syncer.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+require_relative '../missive/post_builder'
+require_relative '../missive/queue'
+require_relative '../streams/timesheet_stream'
+require_relative 'overview_refresher'
+
 class TimesheetsSyncer
   def initialize(qbt, repos, cursor)
     @stream = TimesheetStream.new(qbt_client: qbt, cursor_store: cursor, limit: Constants::QBT_PAGE_LIMIT)
@@ -8,14 +13,19 @@ class TimesheetsSyncer
 
   def backfill_all(&done)
     touched = {}
-    @stream.each_batch do |rows|
+    @stream.each_batch(proc do |rows|
       rows.each do |ts|
         changed = @ts_repo.upsert(ts)
         touched[ts['jobcode_id']] = true if changed
-        Missive::Queue.enqueue(Missive::PostBuilder.timesheet_event(ts)) if changed
+        QuickbooksTime::Missive::Queue.enqueue(QuickbooksTime::Missive::PostBuilder.timesheet_event(ts)) if changed
+      end
+    end) do |ok|
+      if ok
+        OverviewRefresher.rebuild_many(touched.keys) { done&.call(true) }
+      else
+        done&.call(false)
       end
     end
-    OverviewRefresher.rebuild_many(touched.keys) { done&.call(true) }
   rescue StandardError => e
     LOG.error [:timesheet_sync_failed, e.message]
     done&.call(false)
